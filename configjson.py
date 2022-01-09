5c3af867c4ae9f3b6526e6f05b6e16130391acf4
edgarpecero <81655331+edgarpecero@users.noreply.github.com>
2022-01-09 02:05:24 -0600

    Add files via upload

---
+++ configjson.py
@@ -0,0 +1,11 @@
+
+import json
+config = {
+  'name': 'value'
+}
+# with open('config.json', 'w') as f: #WRITE
+#   json.dump(config, f)
+
+# with open('config.json', 'r') as f: #READ
+#   config = json.load(f)
+#   print(config['name'])
